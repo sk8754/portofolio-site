d17bfa355270f11bf8c8347e7a0c4cf1c7c60b50
sk8754 <kishi8755@gmail.com>
2024-09-05 13:21:32 +0900

    更新

--- components/component/component.tsx
+++ components/component/component.tsx
@@ -94,22 +94,6 @@ export function Component() {
               <br />
               また小規模なアプリ開発、システム開発であればフルスタックで開発ができます。
             </p>
-            <div className="flex gap-4">
-              <Link
-                href="#"
-                className="inline-flex items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
-                prefetch={false}
-              >
-                デザイン
-              </Link>
-              <Link
-                href="#"
-                className="inline-flex items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
-                prefetch={false}
-              >
-                プログラム
-              </Link>
-            </div>
           </div>
         </section>
         <section className="mt-12">
